f6f477615c63952e3c6cca8a52521a26c48d6189
yes3421 <karl_heinrich1883@outlook.com>
2022-10-09 23:54:02 +0000

    Added tasks

--- main.cpp
+++ main.cpp
@@ -12,5 +12,13 @@ int main(int argc, char* argv[])
 
     std::cout << records << '\n';
 
+    // Ordenar por ip
+
+    // Generar archivo
+
+    // Solicitar las ip
+
+    // Desplegar y guardar archivo
+
     return EXIT_SUCCESS;
 }
