d9c3f0979eaa1d523b04e8b6c0580b93f9f28969
yes3421 <karl_heinrich1883@outlook.com>
2022-10-11 04:07:16 +0000

    Added data

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,3 +1,18 @@
+/*
+ * Actividad Integral estructura de datos lineales
+ *
+ * Actividad 2.3
+ *
+ * Fernando Gabriel Gutiérrez Madrigal A01424790
+ * Omar Adalid Macías Mayorquín A01635795
+ * Paul Enrique Alonso Ramírez A01634608
+ * Programación de estructuras de datos y algoritmos fundamentales
+ * Grupo 614
+ * Equipo 5
+ * Tecnológico de Monterrey, Campus Guadalajara
+ * 10 de octubre de 2022
+ */
+ 
 #include "LinkedList.h"
 
 LinkedList::LinkedList()
